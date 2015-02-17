4839871808059932508dc301d3c10ad108876edd
William Johnston <william@johnstonhaus.us>
2015-02-17 11:34:02 -0600

    Use the faye-websocket layer.

--- server/app.js
+++ server/app.js
@@ -1,19 +1,29 @@
 (function(){
     'use strict';
 
-    var faye = require('faye');
+    var WebSocket = require('faye-websocket');
     var http = require('http');
 
-    var server = http.createServer(),
-    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+    var server = http.createServer();
 
-    bayeux.attach(server);
+    server.on('upgrade', function(request, socket, body) {
+        if (WebSocket.isWebSocket(request)) {
+            var ws = new WebSocket(request, socket, body);
+
+            // ws.on('message', function(event) {
+            //   ws.send(event.data);
+            // });
+
+            setInterval(function(){
+                ws.send(new Date().toLocaleTimeString());
+            }, 1000);
+
+            ws.on('close', function(event) {
+                console.log('close', event.code, event.reason);
+                ws = null;
+            });
+        }
+    });
+
     server.listen(8000);
-
-    var client = bayeux.getClient();
-    setInterval(function(){
-        client.publish('/time', {
-            currentTime: new Date().toLocaleTimeString()
-        });
-    }, 1000);
 })();
