7958324f416308a52899ef0a565bce3a2356d36a
William Johnston <william@johnstonhaus.us>
2015-02-17 11:46:00 -0600

    Socket.io example.

--- server/app.js
+++ server/app.js
@@ -1,19 +1,20 @@
 (function(){
     'use strict';
 
-    var faye = require('faye');
     var http = require('http');
+    var server = http.createServer();
+    var io = require('socket.io')(server);
 
-    var server = http.createServer(),
-    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 
-    bayeux.attach(server);
-    server.listen(8000);
+    io.on('connection', function(socket){
+        console.log('a user connected');
+    });
 
-    var client = bayeux.getClient();
+    server.listen(8000, function(){
+        console.log('listening on *:8000');
+    });
+
     setInterval(function(){
-        client.publish('/time', {
-            currentTime: new Date().toLocaleTimeString()
-        });
+        io.emit('time', new Date().toLocaleTimeString());
     }, 1000);
 })();
